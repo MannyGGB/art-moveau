deb7b550c8e72f1cd2a87033bdb43c3234039298
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-03 15:39:52 +0000

    Add movie functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
+  const [poster, setPoster] = useState("");
+  const [search, setSearch] = useState("");
 
+  async function handleSearch(event) {
+    event.preventDefault();
+    const movieAPI = `http://localhost:8080/movie?search=${search}`;
+
+    const res = await axios.get(movieAPI);
+    setTitle(res.data.title);
+    setPoster(res.data.poster);
+  }
   return (
-    <>
+    <main>
+      <form onSubmit={handleSearch}>
+        <input
+          placeholder="Movie title"
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button>Search</button>
+      </form>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>{title}</p>
+        <img src={poster} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -7,5 +7,26 @@ app.use(cors());
 const axios = require("axios");
 
 // add your endpoints here
-app.get("/", (request, response) => response.json("Root route for translatim"));
+app.get("/", (request, response) => response.json("Root route for art-moveau"));
+
+app.get("/movie", async (request, response) => {
+  const { search } = request.query;
+  const movieAPI = `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${process.env.TMDB_ACCESS_KEY}`;
+  //different way of using axios to get data
+  const options = {
+    method: "GET",
+    url: movieAPI,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.BEARER}`,
+    },
+  };
+  const res = await axios.request(options);
+
+  const wrangledData = {
+    title: res.data.results[0].title,
+    poster: `https://image.tmdb.org/t/p/w500${res.data.results[0].poster_path}`,
+  };
+  response.json(wrangledData);
+});
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
